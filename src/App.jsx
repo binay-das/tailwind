3d6c997fc91c1d7d37ef7e4f19689902b6a11d64
Binay <binaydas1123@gmail.com>
2025-01-06 19:23:38 +0530

    Routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Screen1 from './Pages/Screen1'
 import Screen2 from './Pages/Screen2'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Screen1 />
-      <Screen2 />
-    </>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Screen1 />} />
+      <Route path="/email" element={<Screen2 />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
