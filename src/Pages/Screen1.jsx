eec4c361774f2948e04afbb0deb1b618ea4c0c6e
Binay <binaydas1123@gmail.com>
2025-01-07 09:20:14 +0530

    Styling changes

--- src/Pages/Screen1.jsx
+++ src/Pages/Screen1.jsx
@@ -7,15 +7,15 @@ const Screen1 = () => {
         setDob(e.target.value);
     }
 
-    return <div className="bg-blue-500 min-h-screen flex flex-col items-center text-center">
-    <h1 className="text-webinar text-blue-900 mt-32 text-6xl font-semibold">
+    return <div className="bg-mainbg min-h-screen flex flex-col items-center text-center">
+      <h1 className="text-webinar mt-32 text-6xl font-semibold">
       Webinar
-      <span>.gg</span>
+      <span className="text-maintext">.gg</span>
     </h1>
-    <h1 className="mt-20 text-4xl font-medium">
+    <h1 className="text-maintext mt-20 text-4xl font-medium">
       Verify your age
     </h1>
-    <p className="mt-12 mb-4 text-xs opacity-50">
+    <p className="text-smallp mt-12 mb-4 text-xs opacity-50">
       Please confirm your birth year. This data will not be stored.
     </p>
 
