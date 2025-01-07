0e977d20e988d871a26d64d8af42e55e31640193
Binay <binaydas1123@gmail.com>
2025-01-07 09:13:04 +0530

    config colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mainbg': '#00274e',
+        'webinar': '#71beda',
+        'inputboxbg': '#18395f',
+        'inputbox': '#ccebff',
+        'maintext': '#d7f2ff',
+        'smallp': '#d7f2ff',
+        'buttonbgdisabled': '#738aa1',
+        'buttonbgenabled': '#3cdbc9',
+        'buttoncolordisabled': '#dcf0ff',
+        'buttoncolorenabled': 'black',
+
+
+        'optionsbg': '#67D5C7',
+
+      }
+    },
   },
   plugins: [],
 }
