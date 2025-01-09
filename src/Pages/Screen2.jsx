e246a7ac69af5eed675337255aae2aee8add0c00
Binay <binaydas1123@gmail.com>
2025-01-09 11:40:55 +0530

    Functionality done

--- src/Pages/Screen2.jsx
+++ src/Pages/Screen2.jsx
@@ -1,6 +1,25 @@
 import { useState } from "react";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+import { useNavigate } from "react-router-dom";
+import { isValidEmail } from "../utils";
 
 export default function Screen2() {
+  const [email, setEmail] = useState("");
+  const [isValid, setIsValid] = useState(null);
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setEmail(e.target.value);
+    setIsValid(isValidEmail(email));
+  };
+
+  const onClick = () => {
+    if (isValid) {
+      navigate("/otp");
+    }
+  };
+
   return (
     <div className="bg-blue min-h-screen flex flex-col items-center ">
       <h1 className="mt-32 text-6xl">
@@ -9,10 +28,12 @@ export default function Screen2() {
       </h1>
       <h1 className="m-20 text-4xl font-medium">Let's get started</h1>
 
-      <div className="flex flex-col items-center">
-        <input placeholder={"Email Id"} type={"email"} />
+      <div className="flex flex-col items-center gap-4 sm:w-96 w-full">
+        <Input placeholder={"Email Id"} type={"email"} onChange={onChange} />
 
-        <button>Continue</button>
+        <Button disabled={!email || !isValid} onClick={onClick}>
+          Continue
+        </Button>
       </div>
     </div>
   );
