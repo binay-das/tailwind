343ecf5dc692ef373faa755a8891e47cbd2ba0f9
Binay <binaydas1123@gmail.com>
2025-01-11 10:04:00 +0530

    SubOtp Components rendered

--- src/Pages/Screen3.jsx
+++ src/Pages/Screen3.jsx
@@ -1,14 +1,27 @@
+import { useRef } from "react";
 import { Button } from "../components/Button";
+import { SubOtpBox } from "../components/SubOtpBox";
 
 export const Screen3 = () => {
+  const inputRefs = useRef([]);
   return (
     <div className="bg-mainbg min-h-screen flex justify-center items-center text-center">
-        <div className="max-w-[350px] flex flex-col gap-8">
+      <div className="max-w-[350px] flex flex-col gap-8">
         <div className="flex justify-center items-center gap-4">
-            <input type="number" />
+
+          {Array.from({ length: 6 }).map((_, index) => (
+            <SubOtpBox
+              key={index}
+              ref={(el) => (inputRefs.current[index] = el)}
+              
+            />
+
+          ))}
+
+
         </div>
         <Button>Verify</Button>
-        </div>
+      </div>
     </div>
   );
 };
