f10ca104d27fc2448843eae9a2e9d4bbf4b6e3fc
Binay <binaydas1123@gmail.com>
2025-01-12 09:06:51 +0530

    Otp box functionality added

--- src/Pages/Screen3.jsx
+++ src/Pages/Screen3.jsx
@@ -1,9 +1,35 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Button } from "../components/Button";
 import { SubOtpBox } from "../components/SubOtpBox";
 
 export const Screen3 = () => {
+  const [otpValues, setOtpValues] = useState(Array(6).fill(""));
+  const [disabled, setDisabled] = useState(true);
   const inputRefs = useRef([]);
+
+  const handleChange = (index) => (e) => {
+    const value = e.target.value;
+    const updatedValues = [...otpValues];
+    updatedValues[index] = value;
+    setOtpValues(updatedValues);
+
+    if (e.target.value && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+
+    setDisabled(!updatedValues.every((val) => val.trim() !== ""));
+
+    if (val && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const handleBackspace = (index) => () => {
+    if (index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+  
   return (
     <div className="bg-mainbg min-h-screen flex justify-center items-center text-center">
       <div className="max-w-[350px] flex flex-col gap-8">
@@ -13,14 +39,15 @@ export const Screen3 = () => {
             <SubOtpBox
               key={index}
               ref={(el) => (inputRefs.current[index] = el)}
-              
+              onChange={handleChange(index)}
+              onBackspace={handleBackspace(index)}
             />
 
           ))}
 
 
         </div>
-        <Button>Verify</Button>
+        <Button  disabled={disabled}>Verify</Button>
       </div>
     </div>
   );
