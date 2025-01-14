4a9bf6907f57fc1eec8479c218f1b4942243d7c0
Binay <binaydas1123@gmail.com>
2025-01-14 09:43:47 +0530

    Dashboard page

---
+++ src/Pages/Dashboard.jsx
@@ -0,0 +1,20 @@
+import Sidebar from "../components/dashboard/Sidebar";
+
+const Dashboard = () => {
+  const toggleTheme = () => {
+    document.querySelector("html").classList.toggle("dark");
+  };
+  return (
+    <div className="flex w-full h-screen ">
+      <h1
+        className="border p-2 text-gray-50 inline rounded-lg m-1 cursor-pointer absolute right-0"
+        onClick={toggleTheme}
+      >
+        Toggle Theme
+      </h1>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Dashboard;
