8e7e82710b292c25b1dcc22c473a4f35fb1619db
Binay <binaydas1123@gmail.com>
2025-01-17 12:30:06 +0530

    Dashboard continuation

--- src/components/dashboard/Content.jsx
+++ src/components/dashboard/Content.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import OptionsComponent from "./OptionsComponent";
 import ProfilePic from "./ProfilePic";
 
 const Content = () => {
+  const [greeting, setGreeting] = useState("");
+  const [currentDate, setCurrentDate] = useState("");
+
   return (
     <div className="w-full flex flex-col min-h-screen">
       <div className="bg-[#11272D] h-44 w-full"></div>
+
       <div className="flex flex-col flex-grow md:grid grid-cols-12 gap-4 pl-10 pr-2 w-full dark:bg-[#021b33] bg-white text-black dark:text-white overflow-visible  transition-all duration-200 ease-in">
         <div className="bg-white dark:bg-gray-700 flex flex-col justify-center items-center h-96 col-span-3 shadow-2xl -translate-y-12 rounded-xl z-10 cursor-pointer">
           <ProfilePic
@@ -18,6 +23,13 @@ const Content = () => {
           <p className="mt-4 opacity-50">prabhleen@gmail.com</p>
           <p className="opacity-50">+91 XXXXXXXXXX</p>
           <p className="mt-4 opacity-50">Delhi, India</p>
+        </div>
+        <div className="col-span-9">
+          <div className="p-4 font-bold ">
+            <p className="opacity-50">{currentDate}</p>
+            <h1 className=" text-2xl ">{greeting}, Prabhleen!</h1>
+          </div>
+
         </div>
       </div>
 
